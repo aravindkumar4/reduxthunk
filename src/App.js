4d2d2ee3e1c179200c1e9522493294e051fceb73
aravind <aravindmailinfo@gmail.com>
2020-07-23 23:31:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// import {
+//   getVadiveluComedyAction,
+//   getgOWNDAMANYComedyAction,
+//   subscribeAction,
+// } from './index';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const myVariable = [this.props.data];
+
+    var ar = myVariable[0];
+    console.log(ar);
+    if (Object.keys(myVariable) === 'data') {
+      console.log(Object.values(myVariable));
+    }
+    return (
+      <>
+        <h3>redux thunk</h3>
+        <p>loading: {this.props.loading ? 'true' : 'false'}</p>
+        <p>data: {JSON.stringify(this.props.data)}</p>
+
+        <img src={this.props.data} alt='' />
+
+        <p>error: {this.props.error}</p>
+        <button onClick={() => this.props.login()}>Login</button>
+        {/* <img src={this.props.img} />;
+        <button
+          disabled={!this.props.isSubscribed}
+          onClick={() => this.props.vadiveluComedy()}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Vadivelu comedy
+        </button>
+        &nbsp;
+        <button
+          disabled={!this.props.isSubscribed}
+          onClick={() => this.props.goundamanyComedy()}
+        >
+          gowndamany comedy
+        </button>
+        &nbsp;
+        <button onClick={() => this.props.subscribe()}>
+          {!this.props.isSubscribed ? 'Subscribe' : 'Unsubscribe'}
+        </button> */}
+      </>
+    );
+  }
+}
+const middleware = () => {
+  return (dispatch) => {
+    dispatch({ type: 'LOGIN_START' });
+    const urls =
+      'https://eatoo.uberdoo.com/foodapp/public/api/guest/listRestaurant';
+    axios
+      .post(urls, { latitude: 13.012345, longitude: 80.123456 })
+
+      .then((res) => {
+        // console.log(res.data.banners[0].bannerImage);
+        dispatch({
+          type: 'LOGIN_SUCCESS',
+          payload: res.data.banners[0].bannerImage,
+        });
+      });
+
+    // .then((response) => response.json())
+    // .then((json) => dispatch({ type: 'LOGIN_SUCCESS', payload: json }));
+  };
+};
+function mapStateToProps(state) {
+  console.log(state);
+  return state;
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    login: () => dispatch(middleware()),
+    // goundamanyComedy: () => dispatch(getgOWNDAMANYComedyAction()),
+    // subscribe: () => dispatch(subscribeAction()),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(App);
